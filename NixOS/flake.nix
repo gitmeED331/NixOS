77b695d320a6b7c119bc239c7c7a2c9d1b88b444
Topsy Krypts <142960718+gitmeED331@users.noreply.github.com>
2024-11-09 13:51:42 -0600

    Add files via upload

---
+++ NixOS/flake.nix
@@ -0,0 +1,74 @@
+{
+  description = ''
+    A Nix flake for the Hyprland window manager.
+    <https://github.com/hyprwm/hyprland>
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    # <https://github.com/nix-systems/nix-systems>
+    systems.url = "github:nix-systems/default-linux";
+
+    ags = {
+      url = "github:aylur/ags/v2";
+    };
+
+    hyprland = {
+      url = "github:hyprwm/hyprland";
+      inputs.systems.follows = "systems";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland-protocols = {
+      url = "github:hyprwm/hyprland-protocols";
+      inputs.systems.follows = "systems";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland-xdph = {
+      url = "github:hyprwm/xdg-desktop-portal-hyprland";
+      inputs.systems.follows = "systems";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.hyprland-protocols.follows = "hyprland-protocols";
+    };
+    hyprlang = {
+      url = "github:hyprwm/hyprlang";
+      inputs.systems.follows = "systems";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = inputs@{ self, nixpkgs, systems, hyprland, hyprland-protocols, 
+    hyprland-xdph, home-manager, ags, ... }: 
+    let
+      system = "x86_64-linux";
+    in let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+     extraSpecialArgs = { inherit inputs; };
+    nixosConfigurations = {
+      Dragon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # home-manager.users.topsykrets = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+          
+        ];
+      };
+    };
+};
+}
---
+++ NixOS/home.nix
@@ -0,0 +1,194 @@
+{ config, pkgs, settings, inputs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "${settings.username}";
+  home.homeDirectory = "/home/topsykrets";
+
+services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+}
+
+  networking.hostName = "Dragon";
+  
+   wayland.windowManager.hyprland = {
+    enable = true;
+    package = pkgs.hyprland;
+    xwayland.enable = true;
+    systemd.enable = true;
+    systemd.variables = ["--all"];
+  };
+
+  home.sessionVariables.NIXOS_OZONE_WL = "1";
+  
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    #"Xft.dpi" = 172;
+  };
+
+ xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+};
+
+home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Liberation Mono";
+      size = 10;
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+ home.packages = with pkgs; [
+    # apps
+    enpass
+	  keepassxc
+    
+    # archive
+    zip
+    xz
+    unzip
+    p7zip
+
+    # web browser
+    vivaldi
+	  vivaldi-ffmpeg-codecs
+
+    # coding
+	  vscode
+    lsd
+    gawk
+    terminator
+	  
+    # comms
+    vesktop
+	  signal-desktop
+    threema-desktop
+    zoom-us
+    
+    # utils
+    git
+    wget
+    htop
+    udiskie
+    xdg-desktop-portal-hyprland
+	  light
+	  xdg-utils  
+    rofi
+  	wl-clipboard
+	  hyprpicker
+	  swww
+	  slurp
+	  grim
+	  grimblast
+	  wf-recorder
+    cliphist
+    
+    # drivers
+    hplipWithPlugin
+    openrazer-daemon
+
+    # office
+		libreoffice-fresh
+	  cryptomator
+	  pcloud
+	  gnome.nautilus
+
+    # security
+    hyprlock
+    protonvpn-gui
+	  greetd.regreet
+	  greetd.greetd	
+		gnome.seahorse
+		polkit_gnome
+
+    # AGS
+	  # inputs.ags.packages.${pkgs.system}.io 
+    # inputs.ags.packages.${pkgs.system}.notifd 
+    # inputs.ags.packages.${pkgs.system}.wireplumber
+    # inputs.ags.packages.${pkgs.system}.hyprland
+    # inputs.ags.packages.${pkgs.system}.river
+    # inputs.ags.packages.${pkgs.system}.apps
+    # inputs.ags.packages.${pkgs.system}.battery
+    # inputs.ags.packages.${pkgs.system}.bluetoot
+    # inputs.ags.packages.${pkgs.system}.network
+    # inputs.ags.packages.${pkgs.system}.mpris
+    # inputs.ags.packages.${pkgs.system}.tray
+    # inputs.ags.packages.${pkgs.system}.cava
+    # inputs.ags.packages.${pkgs.system}.auth
+    # inputs.ags.packages.${pkgs.system}.greet 
+  ];
+
+programs.git = {
+    enable = true;
+    userName = "topsykrets";
+    userEmail = "topsykrets@proton.me";
+  };
+
+  # add the home manager module
+  imports = [ inputs.ags.homeManagerModules.default ];
+
+programs.ags = {
+    enable = true;
+    configDir = ../ags;
+
+    # additional packages to add to gjs's runtime
+    extraPackages = with pkgs; [
+      inputs.ags.packages.${pkgs.system}.battery
+      # inputs.ags.packages.${pkgs.system}.io
+      inputs.ags.packages.${pkgs.system}.notifd
+      inputs.ags.packages.${pkgs.system}.wireplumber
+      inputs.ags.packages.${pkgs.system}.hyprland
+      inputs.ags.packages.${pkgs.system}.river
+      inputs.ags.packages.${pkgs.system}.apps
+      inputs.ags.packages.${pkgs.system}.bluetooth
+      inputs.ags.packages.${pkgs.system}.network
+      inputs.ags.packages.${pkgs.system}.mpris
+      inputs.ags.packages.${pkgs.system}.tray
+      inputs.ags.packages.${pkgs.system}.cava
+      inputs.ags.packages.${pkgs.system}.auth
+      inputs.ags.packages.${pkgs.system}.greet
+      fzf
+    ];
+  };
+
